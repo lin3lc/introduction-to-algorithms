6289defc156b6d8856416234b83ea1178269a0a0
eric <woshilino521@qq.com>
2020-09-02 19:27:14 +0800

    randomized_quicksort

--- 5_quicksort.c
+++ 5_quicksort.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void swap(int *a, int *b);
 int partition(int *a, int p, int r);
+int randomized_partition(int *a, int p, int r);
 void quicksort(int *a, int p, int r);
 
 int main(int argc, char *argv[]){
@@ -41,9 +43,17 @@ int partition(int *a, int p, int r){
     return i+1;
 }
 
+int randomized_partition(int *a, int p, int r){
+    srand((unsigned)time(NULL));
+    int x = rand() % (r-p+1);
+    swap(a+x, a+r);
+
+    return partition(a, p, r);
+}
+
 void quicksort(int *a, int p, int r){
     if(r>p){
-        int q = partition(a, p, r);
+        int q = randomized_partition(a, p, r);
         quicksort(a, p, q-1);
         quicksort(a, q+1, r);
     }
